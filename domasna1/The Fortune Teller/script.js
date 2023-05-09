52f4a11ed889457b6da46e0aa118d64755a1f5c4
Aleksandar <vaaleksandar@gmail.com>
2023-05-09 15:13:51 +0200

    domasna cas2

---
+++ domasna1/The Fortune Teller/script.js
@@ -0,0 +1,13 @@
+//The Fortune Teller
+// Store the following into variables: number of children, partner's name, geographic location, job title.
+// Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+
+var numChilds = '6';
+var partnerName = 'Vesna';
+var geoLocation = 'Tokio';
+var jobTitle = 'Webdeveloper';
+
+
+console.log("You will be" + "  " + jobTitle + " " + "in"+ " " + geoLocation + " " + "you will be married to"+" " + partnerName+" "+ "and you will have" + " "+ numChilds+ " "+"children." );
+
+
